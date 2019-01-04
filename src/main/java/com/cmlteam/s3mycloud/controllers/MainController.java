5ca75652798bf921bad6f19da696e320620dc406
xonix <xonixx@gmail.com>
2019-01-05 00:38:50 +0200

    cleanup

--- src/main/java/com/cmlteam/s3mycloud/controllers/MainController.java
+++ src/main/java/com/cmlteam/s3mycloud/controllers/MainController.java
@@ -1,16 +1,11 @@
 package com.cmlteam.s3mycloud.controllers;
 
-import com.cmlteam.s3mycloud.model.ServerStatus;
 import com.cmlteam.s3mycloud.services.SampleService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
@@ -26,26 +21,5 @@ public class MainController {
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String test() {
     return "Hello CML!";
-  }
-
-  @RequestMapping(value = "/testws", method = RequestMethod.GET)
-  public String testws() {
-    RestTemplate template = new RestTemplate();
-    ServerStatus status =
-        template.getForObject("https://l2c1x1.com/services/misc/server-stats", ServerStatus.class);
-    return "" + status.totalAccounts;
-  }
-
-  @RequestMapping(value = "/testdb", method = RequestMethod.GET)
-  public String testdb() {
-    return sampleService.getDbVersion();
-  }
-
-  @PostMapping(value = "/testpost")
-  public Map testPost(@RequestBody Map payload) {
-    log.info("Test POST: {}", payload);
-    HashMap res = new HashMap();
-    res.put("success", true);
-    return res;
   }
 }
--- src/main/java/com/cmlteam/s3mycloud/model/ServerStatus.java
+++
@@ -1,8 +0,0 @@
-package com.cmlteam.s3mycloud.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class ServerStatus {
-	public int totalAccounts;
-}
