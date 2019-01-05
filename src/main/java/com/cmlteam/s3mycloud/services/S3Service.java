ac4df3488c9cd41c1b54dcc2d68c48ea80335ac2
xonix <xonixx@gmail.com>
2019-01-05 04:40:26 +0200

    s3 upload POC

--- src/main/java/com/cmlteam/s3mycloud/services/S3Service.java
+++ src/main/java/com/cmlteam/s3mycloud/services/S3Service.java
@@ -1,16 +1,26 @@
 package com.cmlteam.s3mycloud.services;
 
 import com.cmlteam.s3mycloud.S3Props;
+import com.cmlteam.util.Util;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.net.URI;
 
 @Service
 @Slf4j
 public class S3Service {
   private final S3Props s3Props;
+
+  private S3Client s3Client;
 
   @Autowired
   public S3Service(S3Props s3Props) {
@@ -20,5 +30,24 @@ public class S3Service {
   @PostConstruct
   public void postConstruct() {
     log.info("S3 Props: {}", s3Props);
+    s3Client =
+        S3Client.builder()
+            .region(Region.of(s3Props.getRegion()))
+            .endpointOverride(URI.create(s3Props.getEndpoint()))
+            .credentialsProvider(
+                () -> AwsBasicCredentials.create(s3Props.getAccessKey(), s3Props.getSecretKey()))
+            .build();
+    test();
+  }
+
+  private void test() {
+    log.info("Starting upload");
+    long t0 = System.currentTimeMillis();
+    String file = "/home/xonix/Downloads/haproxy_exporter-0.9.0.linux-amd64.tar.gz";
+    String name = "haproxy_exporter-0.9.0.linux-amd64.tar.gz";
+    s3Client.putObject(
+        PutObjectRequest.builder().bucket(s3Props.getBucket()).key(name).build(),
+        RequestBody.fromFile(new File(file)));
+    log.info("Upload took: {}", Util.renderDurationFromStart(t0));
   }
 }
