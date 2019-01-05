23277ca23a744922b1a830580eb5d8c7071628f1
xonix <xonixx@gmail.com>
2019-01-05 04:56:54 +0200

    s3 list POC

--- src/main/java/com/cmlteam/s3mycloud/services/S3Service.java
+++ src/main/java/com/cmlteam/s3mycloud/services/S3Service.java
@@ -9,7 +9,10 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
@@ -37,10 +40,11 @@ public class S3Service {
             .credentialsProvider(
                 () -> AwsBasicCredentials.create(s3Props.getAccessKey(), s3Props.getSecretKey()))
             .build();
-    test();
+    //    testUpload();
+    testList();
   }
 
-  private void test() {
+  private void testUpload() {
     log.info("Starting upload");
     long t0 = System.currentTimeMillis();
     String file = "/home/xonix/Downloads/haproxy_exporter-0.9.0.linux-amd64.tar.gz";
@@ -49,5 +53,39 @@ public class S3Service {
         PutObjectRequest.builder().bucket(s3Props.getBucket()).key(name).build(),
         RequestBody.fromFile(new File(file)));
     log.info("Upload took: {}", Util.renderDurationFromStart(t0));
+  }
+
+  private void testList() {
+    // maxKeys is set to 2 to demonstrate the use of
+    // ListObjectsV2Result.getNextContinuationToken()
+    int maxKeys = 50;
+    ListObjectsV2Request req =
+        ListObjectsV2Request.builder().bucket("nextcloud-xonix-1").maxKeys(maxKeys).build();
+    ListObjectsV2Response resp;
+
+    long totalSize = 0;
+
+    do {
+      resp = s3Client.listObjectsV2(req);
+
+      for (S3Object objectSummary : resp.contents()) {
+        Long size = objectSummary.size();
+        totalSize += size;
+        log.info(" - {} (size: {})", objectSummary.key(), Util.renderFileSize(size));
+      }
+      // If there are more than maxKeys keys in the bucket, get a continuation token
+      // and list the next objects.
+      String token = resp.nextContinuationToken();
+      log.info("Next Continuation Token: {}", token);
+      req =
+          ListObjectsV2Request.builder()
+              .bucket("nextcloud-xonix-1")
+              .continuationToken(token)
+              .maxKeys(maxKeys)
+              .build();
+
+    } while (resp.isTruncated());
+
+    log.info("TOTAL: {}", Util.renderFileSize(totalSize));
   }
 }
