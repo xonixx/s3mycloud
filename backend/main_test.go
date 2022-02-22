1da33e57de713feeae88d500f7571916c90af388
xonix <xonixx@gmail.com>
2022-02-22 23:24:30 +0200

    Cover REST API with E2E tests #14

--- backend/main_test.go
+++ backend/main_test.go
@@ -4,13 +4,15 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
+
+type M map[string]interface{}
 
 func toJson(v interface{}) io.Reader {
 	if bytesData, err := json.Marshal(v); err == nil {
@@ -40,8 +42,8 @@ func checkResponse(t *testing.T, resp *http.Response, err error, expectedStatus 
 	}
 }
 
-func readJsonAsMap(t *testing.T, resp *http.Response) gin.H {
-	var body gin.H
+func readJsonAsMap(t *testing.T, resp *http.Response) M {
+	var body M
 	defer resp.Body.Close()
 	if data, err := ioutil.ReadAll(resp.Body); err != nil {
 		t.Errorf("expected error to be nil got %v", err)
@@ -51,12 +53,12 @@ func readJsonAsMap(t *testing.T, resp *http.Response) gin.H {
 	return body
 }
 
-func checkAndReadRespJson(t *testing.T, resp *http.Response, err error, expectedStatus int) gin.H {
+func checkAndReadRespJson(t *testing.T, resp *http.Response, err error, expectedStatus int) M {
 	checkResponse(t, resp, err, expectedStatus)
 	return readJsonAsMap(t, resp)
 }
 
-func getJsonField(t *testing.T, m gin.H, field string) interface{} {
+func getJsonField(t *testing.T, m M, field string) interface{} {
 	val, ok := m[field]
 	if !ok {
 		t.Fatalf("should contain %s", field)
@@ -66,11 +68,30 @@ func getJsonField(t *testing.T, m gin.H, field string) interface{} {
 	}
 }
 
+func query(v interface{}, path ...string) interface{} {
+	for _, f := range path {
+		if m, ok := v.(map[string]interface{}); ok {
+			v = m[f]
+		} else if m, ok := v.(M); ok {
+			v = m[f]
+		} else if l, ok := v.([]interface{}); ok {
+			if intVal, err := strconv.Atoi(f); err == nil {
+				v = l[intVal]
+			} else {
+				panic("Not an int: " + f)
+			}
+		} else {
+			panic(fmt.Sprintf("Not a map/list: %v", v))
+		}
+	}
+	return v
+}
+
 func TestAddFileTxt(t *testing.T) {
 	ts := httptest.NewServer(setupServer())
 	defer ts.Close()
 
-	resp, err := http.Post(fmt.Sprintf("%s/api/file/upload", ts.URL), "application/json", toJson(gin.H{
+	resp, err := http.Post(fmt.Sprintf("%s/api/file/upload", ts.URL), "application/json", toJson(M{
 		"name": "file.txt",
 		"size": 100,
 		"tags": []string{"text", "document"},
@@ -90,8 +111,8 @@ func TestAddFileTxt(t *testing.T) {
 	if total := int(getJsonField(t, respJson, "total").(float64)); total != 1 {
 		t.Fatalf("wrong total: %d", total)
 	}
-	resultId := getJsonField(t, respJson, "page").([]interface{})[0].(map[string]interface{})["id"].(string)
+	resultId := query(respJson, "page", "0", "id").(string)
 	if id != resultId {
-		t.Fatalf(" resultId %s != id %s", resultId, id)
+		t.Fatalf("resultId %s != id %s", resultId, id)
 	}
 }
