24e169fb20c0a41543f9c9ab660da741c6f85fed
xonix <xonixx@gmail.com>
2022-02-22 21:52:17 +0200

    Cover REST API with E2E tests #14

--- backend/main_test.go
+++ backend/main_test.go
@@ -6,34 +6,26 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func toJson(v interface{}) io.Reader {
-	if bytes_, err := json.Marshal(v); err == nil {
-		return bytes.NewReader(bytes_)
+	if bytesData, err := json.Marshal(v); err == nil {
+		return bytes.NewReader(bytesData)
 	} else {
 		panic(err)
 	}
 }
 
-func TestAddFileTxt(t *testing.T) {
-	ts := httptest.NewServer(setupServer())
-	defer ts.Close()
-
-	resp, err := http.Post(fmt.Sprintf("%s/api/file/upload", ts.URL), "application/json", toJson(gin.H{
-		"name": "file.txt",
-		"size": 100,
-		"tags": []string{"text", "document"},
-	}))
-
+func checkResponse(t *testing.T, resp *http.Response, err error, expectedStatus int) {
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
-	if resp.StatusCode != http.StatusCreated {
+	if resp.StatusCode != expectedStatus {
 		t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
 	}
 
@@ -46,4 +38,30 @@ func TestAddFileTxt(t *testing.T) {
 	if val[0] != "application/json; charset=utf-8" {
 		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
 	}
+}
+
+func readJsonAsMap(t *testing.T, resp *http.Response) gin.H {
+	var body gin.H
+	defer resp.Body.Close()
+	if data, err := ioutil.ReadAll(resp.Body); err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	} else if err := json.Unmarshal(data, &body); err != nil {
+		t.Errorf("Not a JSON, got %v", err)
+	}
+	return body
+}
+
+func TestAddFileTxt(t *testing.T) {
+	ts := httptest.NewServer(setupServer())
+	defer ts.Close()
+
+	resp, err := http.Post(fmt.Sprintf("%s/api/file/upload", ts.URL), "application/json", toJson(gin.H{
+		"name": "file.txt",
+		"size": 100,
+		"tags": []string{"text", "document"},
+	}))
+
+	fmt.Println("resp JSON:", readJsonAsMap(t, resp))
+
+	checkResponse(t, resp, err, http.StatusCreated)
 }
