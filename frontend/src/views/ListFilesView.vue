4df114b089cad9854254784bb9f4ee0768489b81
xonix <xonixx@gmail.com>
2022-07-16 02:50:56 +0300

    Scaffold frontend part #25 add bulma

--- frontend/src/views/ListFilesView.vue
+++ frontend/src/views/ListFilesView.vue
@@ -4,5 +4,20 @@
 <template>
   <main>
     List files goes here...
+
+    <div class="columns">
+      <div class="column">
+        First column
+      </div>
+      <div class="column">
+        Second column
+      </div>
+      <div class="column">
+        Third column
+      </div>
+      <div class="column">
+        Fourth column
+      </div>
+    </div>
   </main>
 </template>
