11fe56c47e22aa752070236770df4ad05f317a96
xonix <xonixx@gmail.com>
2022-07-16 16:15:12 +0300

    Scaffold frontend part #25 search bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,18 +4,24 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-<!--  <header>-->
-<!--    <div class="wrapper">-->
-<!--      <HelloWorld msg="You did it!" />-->
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        S3MyCloud
+      </h1>
+      <RouterView />
+    </div>
+  </section>
+    <!--  <header>-->
+    <!--    <div class="wrapper">-->
+    <!--      <HelloWorld msg="You did it!" />-->
 
-<!--      <nav>-->
-<!--        <RouterLink to="/">Home</RouterLink>-->
-<!--        <RouterLink to="/about">About</RouterLink>-->
-<!--      </nav>-->
-<!--    </div>-->
-<!--  </header>-->
-
-  <RouterView />
+    <!--      <nav>-->
+    <!--        <RouterLink to="/">Home</RouterLink>-->
+    <!--        <RouterLink to="/about">About</RouterLink>-->
+    <!--      </nav>-->
+    <!--    </div>-->
+    <!--  </header>-->
 </template>
 
 <style scoped>
--- frontend/src/views/ListFilesView.vue
+++ frontend/src/views/ListFilesView.vue
@@ -3,21 +3,15 @@
 
 <template>
 <!--  <main>-->
-    List files goes here...
-
-    <div class="columns" style="width: 100%">
-      <div class="column">
-        First column
-      </div>
-      <div class="column">
-        Second column
-      </div>
-      <div class="column">
-        Third column
-      </div>
-      <div class="column">
-        Fourth column
-      </div>
+  <div class="field is-grouped">
+    <div class="control is-expanded">
+      <input class="input" type="text" placeholder="Enter search query...">
     </div>
+    <div class="control">
+      <a class="button is-info">
+        Search
+      </a>
+    </div>
+  </div>
 <!--  </main>-->
 </template>
