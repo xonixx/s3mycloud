fa08c023c9b08e6f89696516d35f158671b01753
xonix <xonixx@gmail.com>
2022-07-16 17:30:07 +0300

    Create file list page #26

--- frontend/src/views/ListFilesView.vue
+++ frontend/src/views/ListFilesView.vue
@@ -14,6 +14,37 @@ for (let i = 0; i < 20; i++) {
   )
 }
 
+/**
+ * Format bytes as human-readable text.
+ *
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ *
+ * @return Formatted string.
+ */
+function humanFileSize(bytes: number, si=false, dp=1) {
+  const thresh = si ? 1000 : 1024;
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + ' B';
+  }
+
+  const units = si
+      ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+      : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+  let u = -1;
+  const r = 10**dp;
+
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+
+  return bytes.toFixed(dp) + ' ' + units[u];
+}
 
 </script>
 
@@ -35,7 +66,7 @@ for (let i = 0; i < 20; i++) {
       <span v-for="t in r.tags" class="tag is-primary is-light">{{ t }}</span>
       <div style="margin-top: -5px">
         <span class="date">{{ r.date }}</span>
-        <span class="size">{{ r.size }}</span>
+        <span class="size">{{ humanFileSize(r.size,true) }}</span>
       </div>
     </div>
   </div>
