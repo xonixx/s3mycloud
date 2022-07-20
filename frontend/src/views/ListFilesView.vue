02be1a34d8170b606a09bd2a99dbc83b333318d8
xonix <xonixx@gmail.com>
2022-07-21 01:04:00 +0300

    Make pagination working #32

--- frontend/src/views/ListFilesView.vue
+++ frontend/src/views/ListFilesView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import {humanFileSize} from "@/assets/lib";
 import Pagination from "@/components/Pagination.vue";
 
 import moment from 'moment';
@@ -41,38 +42,6 @@ function changePage(p: number) {
   console.info("Changing page to ", p)
   router.push({ path: '/', query: { ...router.currentRoute.value.query, page: p } })
   load()
-}
-
-/**
- * Format bytes as human-readable text.
- *
- * @param bytes Number of bytes.
- * @param si True to use metric (SI) units, aka powers of 1000. False to use
- *           binary (IEC), aka powers of 1024.
- * @param dp Number of decimal places to display.
- *
- * @return Formatted string.
- */
-function humanFileSize(bytes: number, si = false, dp = 1) {
-  const thresh = si ? 1000 : 1024;
-
-  if (Math.abs(bytes) < thresh) {
-    return bytes + ' B';
-  }
-
-  const units = si
-      ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-      : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-  let u = -1;
-  const r = 10 ** dp;
-
-  do {
-    bytes /= thresh;
-    ++u;
-  } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
-
-
-  return bytes.toFixed(dp) + ' ' + units[u];
 }
 
 </script>
