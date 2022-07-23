9c655f1fe6c6f51631e439b03cf4fd30cfcb2563
xonix <xonixx@gmail.com>
2022-07-23 18:00:10 +0300

    Fix bug with total pages calculation #38 prettier

--- frontend/src/components/Pagination.vue
+++ frontend/src/components/Pagination.vue
@@ -1,26 +1,24 @@
 <script setup lang="ts">
-
 const props = defineProps<{
-  page: number
-  pageSize: number
-  totalRecords: number
-  changePage: (p: number) => void
-  changePageSize: (ps: number) => void
+  page: number;
+  pageSize: number;
+  totalRecords: number;
+  changePage: (p: number) => void;
+  changePageSize: (ps: number) => void;
 }>();
 
-const pageSizes = [10, 50, 100]
+const pageSizes = [10, 50, 100];
 
 function totalPages(): number {
-  console.info("tr", props.totalRecords, "ps", props.pageSize)
+  console.info("tr", props.totalRecords, "ps", props.pageSize);
   return Math.ceil(props.totalRecords / props.pageSize);
 }
 
 function myChangePage(p: number): void {
   let tot: number;
   if (p < 0) p = 0;
-  else if (p >= (tot = totalPages()))
-    p = tot - 1
-  props.changePage(p)
+  else if (p >= (tot = totalPages())) p = tot - 1;
+  props.changePage(p);
 }
 
 function isFirstPage() {
@@ -30,7 +28,6 @@ function isFirstPage() {
 function isLastPage() {
   return props.page == totalPages() - 1;
 }
-
 </script>
 
 <template>
@@ -38,34 +35,58 @@ function isLastPage() {
   <!--    <div class="column is-2 field is-grouped">-->
   <div class="field is-grouped">
     <div class="control">
-      <button class="button is-small" :disabled="isFirstPage()" @click="myChangePage(0)">
+      <button
+        class="button is-small"
+        :disabled="isFirstPage()"
+        @click="myChangePage(0)"
+      >
         <i class="fas fa-solid fa-angles-left"></i>
       </button>
     </div>
     <div class="control">
-      <button class="button is-small" :disabled="isFirstPage()" @click="myChangePage(page-1)">
+      <button
+        class="button is-small"
+        :disabled="isFirstPage()"
+        @click="myChangePage(page - 1)"
+      >
         <i class="fas fa-solid fa-angle-left"></i>
       </button>
     </div>
     <div class="control" style="margin-right: 0">
-      <input class="input is-small" type="text" placeholder=""
-             :value="page+1"
-             @keydown.enter="myChangePage((parseInt($event.target.value) || 1)-1)"
-             style="width: 50px">
+      <input
+        class="input is-small"
+        type="text"
+        placeholder=""
+        :value="page + 1"
+        @keydown.enter="myChangePage((parseInt($event.target.value) || 1) - 1)"
+        style="width: 50px"
+      />
     </div>
     <div class="control">
       <!--      <input class="input is-small" type="text" disabled :value="'/ ' + totalPages()"-->
       <!--             style="width: 40px; background-color: #fff; border-color: #fff">-->
-      <span class="input is-small" style="width: 50px; background-color: #fff; border:0">/ {{ totalPages() }}</span>
+      <span
+        class="input is-small"
+        style="width: 50px; background-color: #fff; border: 0"
+        >/ {{ totalPages() }}</span
+      >
       <!--      / {{ totalPages }}-->
     </div>
     <div class="control">
-      <button class="button is-small" :disabled="isLastPage()" @click="myChangePage(page+1)">
+      <button
+        class="button is-small"
+        :disabled="isLastPage()"
+        @click="myChangePage(page + 1)"
+      >
         <i class="fas fa-solid fa-angle-right"></i>
       </button>
     </div>
     <div class="control">
-      <button class="button is-small" :disabled="isLastPage()" @click="myChangePage(totalPages()-1)">
+      <button
+        class="button is-small"
+        :disabled="isLastPage()"
+        @click="myChangePage(totalPages() - 1)"
+      >
         <i class="fas fa-solid fa-angles-right"></i>
       </button>
     </div>
@@ -76,9 +97,7 @@ function isLastPage() {
         </select>
       </div>
     </div>
-    <div class="control">
-      per page
-    </div>
+    <div class="control">per page</div>
   </div>
   <!--  </div>-->
 </template>
--- frontend/src/views/ListFilesView.vue
+++ frontend/src/views/ListFilesView.vue
@@ -1,83 +1,103 @@
 <script setup lang="ts">
-import {humanFileSize} from "@/assets/lib";
+import { humanFileSize } from "@/assets/lib";
 import Pagination from "@/components/Pagination.vue";
 
-import moment from 'moment';
-import {onMounted, ref} from "vue";
+import moment from "moment";
+import { onMounted, ref } from "vue";
 import axios from "axios";
 import router from "@/router";
 
-const params = withDefaults(defineProps<{
-  q: string;
-  page: number;
-  pageSize: number;
-}>(), {
-  q: '',
-  page: 0,
-  pageSize: 10
-});
+const params = withDefaults(
+  defineProps<{
+    q: string;
+    page: number;
+    pageSize: number;
+  }>(),
+  {
+    q: "",
+    page: 0,
+    pageSize: 10,
+  }
+);
 
 const totalRecords = ref<number>();
-const searchResults = ref([])
+const searchResults = ref([]);
 
 async function load() {
-  const response = await axios.get(`http://127.0.0.1:8080/api/file?page=${params.page}&pageSize=${params.pageSize}&name=${params.q}`);
-  searchResults.value = response.data.page.map((e) => ({...e, date: moment(e.uploaded).format("D MMM YYYY")}));
+  const response = await axios.get(
+    `http://127.0.0.1:8080/api/file?page=${params.page}&pageSize=${params.pageSize}&name=${params.q}`
+  );
+  searchResults.value = response.data.page.map((e) => ({
+    ...e,
+    date: moment(e.uploaded).format("D MMM YYYY"),
+  }));
   totalRecords.value = response.data.total;
 }
 
-onMounted(load)
+onMounted(load);
 
 function changePage(p: number) {
-  console.info("Changing page to ", p)
-  router.push({path: '/', query: {...router.currentRoute.value.query, page: p}})
-  setTimeout(load, 10)
+  console.info("Changing page to ", p);
+  router.push({
+    path: "/",
+    query: { ...router.currentRoute.value.query, page: p },
+  });
+  setTimeout(load, 10);
 }
 
 function changePageSize(ps: number) {
-  console.info("Changing page size to ", ps)
-  router.push({path: '/', query: {...router.currentRoute.value.query, pageSize: ps, page: 0}})
-  setTimeout(load, 10)
+  console.info("Changing page size to ", ps);
+  router.push({
+    path: "/",
+    query: { ...router.currentRoute.value.query, pageSize: ps, page: 0 },
+  });
+  setTimeout(load, 10);
 }
 
-const qRef = ref()
+const qRef = ref();
 
 function changeQuery(q: string) {
-  console.info("Changing query to ", q)
-  router.push({path: '/', query: {...router.currentRoute.value.query, q, page: 0}})
-  setTimeout(load, 10)
+  console.info("Changing query to ", q);
+  router.push({
+    path: "/",
+    query: { ...router.currentRoute.value.query, q, page: 0 },
+  });
+  setTimeout(load, 10);
 }
-
 </script>
 
 <template>
   <!--  <main>-->
   <div class="field is-grouped">
     <div class="control is-expanded">
-      <input ref="qRef" :value="q" @keydown.enter="changeQuery(qRef.value)"
-             class="input" type="text" placeholder="Enter search query...">
+      <input
+        ref="qRef"
+        :value="q"
+        @keydown.enter="changeQuery(qRef.value)"
+        class="input"
+        type="text"
+        placeholder="Enter search query..."
+      />
     </div>
     <div class="control">
-      <a class="button is-info">
-        Search
-      </a>
+      <a class="button is-info"> Search </a>
     </div>
   </div>
 
-  <div v-if="totalRecords === 0">
-    Nothing found
-  </div>
+  <div v-if="totalRecords === 0">Nothing found</div>
   <div v-else>
     Found records: {{ totalRecords }}
     <Pagination
-        :page="page"
-        :page-size="pageSize"
-        :total-records="totalRecords"
-        :change-page="changePage"
-        :change-page-size="changePageSize"
+      :page="page"
+      :page-size="pageSize"
+      :total-records="totalRecords"
+      :change-page="changePage"
+      :change-page-size="changePageSize"
     />
     <div v-for="r in searchResults" class="res-item">
-      <a :href="`http://127.0.0.1:8080/api/file/${r.id}/dl`" class="name">{{ r.name }}</a>
+      <a :href="`http://127.0.0.1:8080/api/file/${r.id}/dl`" class="name">{{
+        r.name
+      }}</a>
       <span v-for="t in r.tags" class="tag is-primary is-light">{{ t }}</span>
       <div style="margin-top: -5px">
         <span class="date">{{ r.date }}</span>
@@ -85,11 +105,11 @@ function changeQuery(q: string) {
       </div>
     </div>
     <Pagination
-        :page="page"
-        :page-size="pageSize"
-        :total-records="totalRecords"
-        :change-page="changePage"
-        :change-page-size="changePageSize"
+      :page="page"
+      :page-size="pageSize"
+      :total-records="totalRecords"
+      :change-page="changePage"
+      :change-page-size="changePageSize"
     />
   </div>
   <!--  </main>-->
