2fa1a70e51ac14acc6f46f64a9e5a4bdf966b47d
xonix <xonixx@gmail.com>
2022-07-26 00:09:54 +0300

    We should only hide page size selector when totalRecords <= 10 #40

--- frontend/src/components/Pagination.vue
+++ frontend/src/components/Pagination.vue
@@ -29,79 +29,92 @@ function isLastPage(): boolean {
   return props.page == totalPages() - 1;
 }
 
-function isVisible(): boolean {
+function isNavigationVisible(): boolean {
   return props.totalRecords > props.pageSize;
+}
+
+function isPageSizeVisible(): boolean {
+  return props.totalRecords > pageSizes[0];
 }
 </script>
 
 <template>
   <!--  <div class="columns is-centered">-->
   <!--    <div class="column is-2 field is-grouped">-->
-  <div class="field is-grouped" v-if="isVisible()">
-    <div class="control">
-      <button
-        class="button is-small"
-        :disabled="isFirstPage()"
-        @click="myChangePage(0)"
-      >
-        <i class="fas fa-solid fa-angles-left"></i>
-      </button>
-    </div>
-    <div class="control">
-      <button
-        class="button is-small"
-        :disabled="isFirstPage()"
-        @click="myChangePage(page - 1)"
-      >
-        <i class="fas fa-solid fa-angle-left"></i>
-      </button>
-    </div>
-    <div class="control" style="margin-right: 0">
-      <input
-        class="input is-small"
-        type="text"
-        placeholder=""
-        :value="page + 1"
-        @keydown.enter="myChangePage((parseInt($event.target.value) || 1) - 1)"
-        style="width: 50px"
-      />
-    </div>
-    <div class="control">
-      <!--      <input class="input is-small" type="text" disabled :value="'/ ' + totalPages()"-->
-      <!--             style="width: 40px; background-color: #fff; border-color: #fff">-->
-      <span
-        class="input is-small"
-        style="width: 50px; background-color: #fff; border: 0"
-        >/ {{ totalPages() }}</span
-      >
-      <!--      / {{ totalPages }}-->
-    </div>
-    <div class="control">
-      <button
-        class="button is-small"
-        :disabled="isLastPage()"
-        @click="myChangePage(page + 1)"
-      >
-        <i class="fas fa-solid fa-angle-right"></i>
-      </button>
-    </div>
-    <div class="control">
-      <button
-        class="button is-small"
-        :disabled="isLastPage()"
-        @click="myChangePage(totalPages() - 1)"
-      >
-        <i class="fas fa-solid fa-angles-right"></i>
-      </button>
-    </div>
-    <div class="control" style="margin-left: 50px">
-      <div class="select is-small">
-        <select :value="pageSize" @change="changePageSize($event.target.value)">
-          <option v-for="ps in pageSizes" :value="ps">{{ ps }}</option>
-        </select>
+  <div class="field is-grouped">
+    <template v-if="isNavigationVisible()">
+      <div class="control">
+        <button
+          class="button is-small"
+          :disabled="isFirstPage()"
+          @click="myChangePage(0)"
+        >
+          <i class="fas fa-solid fa-angles-left"></i>
+        </button>
       </div>
-    </div>
-    <div class="control">per page</div>
+      <div class="control">
+        <button
+          class="button is-small"
+          :disabled="isFirstPage()"
+          @click="myChangePage(page - 1)"
+        >
+          <i class="fas fa-solid fa-angle-left"></i>
+        </button>
+      </div>
+      <div class="control" style="margin-right: 0">
+        <input
+          class="input is-small"
+          type="text"
+          placeholder=""
+          :value="page + 1"
+          @keydown.enter="
+            myChangePage((parseInt($event.target.value) || 1) - 1)
+          "
+          style="width: 50px"
+        />
+      </div>
+      <div class="control">
+        <!--      <input class="input is-small" type="text" disabled :value="'/ ' + totalPages()"-->
+        <!--             style="width: 40px; background-color: #fff; border-color: #fff">-->
+        <span
+          class="input is-small"
+          style="width: 50px; background-color: #fff; border: 0"
+          >/ {{ totalPages() }}</span
+        >
+        <!--      / {{ totalPages }}-->
+      </div>
+      <div class="control">
+        <button
+          class="button is-small"
+          :disabled="isLastPage()"
+          @click="myChangePage(page + 1)"
+        >
+          <i class="fas fa-solid fa-angle-right"></i>
+        </button>
+      </div>
+      <div class="control" style="margin-right: 50px">
+        <button
+          class="button is-small"
+          :disabled="isLastPage()"
+          @click="myChangePage(totalPages() - 1)"
+        >
+          <i class="fas fa-solid fa-angles-right"></i>
+        </button>
+      </div>
+    </template>
+    <template v-if="isPageSizeVisible()">
+      <div class="control">
+        <div class="select is-small">
+          <select
+            :value="pageSize"
+            @change="changePageSize($event.target.value)"
+          >
+            <option v-for="ps in pageSizes" :value="ps">{{ ps }}</option>
+          </select>
+        </div>
+      </div>
+      <div class="control">per page</div>
+    </template>
   </div>
   <!--  </div>-->
 </template>
